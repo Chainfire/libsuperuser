6bb42a0226d355f5ae01d5ac449e74aa0d571179
Jorrit Jongma <git@jongma.org>
2015-10-07 11:50:15 +0200

    Add Toolbox utility class

--- libsuperuser/src/eu/chainfire/libsuperuser/ShellOnMainThreadException.java
+++ libsuperuser/src/eu/chainfire/libsuperuser/ShellOnMainThreadException.java
@@ -25,6 +25,7 @@ public class ShellOnMainThreadException extends RuntimeException {
     public static final String EXCEPTION_COMMAND = "Application attempted to run a shell command from the main thread";
     public static final String EXCEPTION_NOT_IDLE = "Application attempted to wait for a non-idle shell to close on the main thread";
     public static final String EXCEPTION_WAIT_IDLE = "Application attempted to wait for a shell to become idle on the main thread";
+    public static final String EXCEPTION_TOOLBOX = "Application attempted to init the Toolbox class from the main thread";
 
     public ShellOnMainThreadException(String message) {
         super(message);
