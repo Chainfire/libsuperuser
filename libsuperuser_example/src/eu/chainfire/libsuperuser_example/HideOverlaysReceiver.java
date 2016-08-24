40c97490c0a52860855532a9a3058a197b13d0e5
pyler <pyler@azet.sk>
2016-08-24 14:50:12 +0200

    Updated example

--- libsuperuser_example/src/eu/chainfire/libsuperuser_example/HideOverlaysReceiver.java
+++ libsuperuser_example/src/eu/chainfire/libsuperuser_example/HideOverlaysReceiver.java
@@ -1,8 +1,11 @@
 package eu.chainfire.libsuperuser_example;
 
+import android.content.Context;
+import android.content.Intent;
+
 public class HideOverlaysReceiver extends eu.chainfire.libsuperuser.HideOverlaysReceiver {
     @Override
-    public void onHideOverlays(boolean hide) {
+    public void onHideOverlays(Context context, Intent intent, boolean hide) {
         if (hide) {
             // hide overlays, if any
         } else {
